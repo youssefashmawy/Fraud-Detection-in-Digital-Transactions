8e70bb8ed4874cc6786d0f9909049f3ecc45cd7f
yousef-moustafa <ymoustafa005@gmail.com>
2024-08-22 19:46:10 +0300

    Database connection initiated

---
+++ database/database.py
@@ -0,0 +1,13 @@
+import firebase_admin
+from firebase_admin import credentials, db
+
+def connect_db():
+    # Load credentials and initialize the Firebase app
+    cred = credentials.Certificate("database/credentials.json")
+    firebase_admin.initialize_app(cred, {
+        'databaseURL': 'https://frauddetection-147b7-default-rtdb.europe-west1.firebasedatabase.app/'
+    })
+
+    # Return the reference to the root of the database or a specific node
+    ref = db.reference('/')
+    return ref
